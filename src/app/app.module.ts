59a36cc5cc53b0d7a1f8ef270efa01035cf85b67
SavioSA <saviosilvadearaujo2019@gmail.com>
2021-10-06 20:22:19 -0300

    initial settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
