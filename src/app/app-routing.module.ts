30641bc050c44d0a7d7a144bfe09ec038dc53417
SavioSA <saviosilvadearaujo2019@gmail.com>
2021-10-09 10:55:22 -0300

    adjust navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./modules/welcome/welcome.module').then(m => m.WelcomeModule)
+  },
+  {
     path: 'trafic-light',
     loadChildren: () => import('./modules/trafic-light/trafic-light.module').then(m => m.TraficLightModule)
   },
   {
     path: 'welcome',
     loadChildren: () => import('./modules/welcome/welcome.module').then(m => m.WelcomeModule)
-  }
+  },
+  { path: '**', redirectTo: 'welcome' }
 ];
 
 @NgModule({
